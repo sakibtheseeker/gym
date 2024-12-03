3fafc461bb77e30ae263662949a5da5c981016dd
sakibtheseeker <stsakib123@gmail.com>
2024-12-03 19:33:58 +0530

    first commit

---
+++ include/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer-bottom">
+            <div class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <div class="copyright">GYM Management System</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
